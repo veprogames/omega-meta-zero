bef3197249367422b32bba512fcbff10c380589b
veprogames <75524847+veprogames@users.noreply.github.com>
2024-06-25 20:27:34 +0200

    new point increase function and move stuff around to fix console errors

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
     import { game } from "./lib/stores";
     import { onDestroy, onMount } from "svelte";
-    import { tickGame } from "./lib/game/game";
-    import LayerResource from "./lib/layers/LayerResource.svelte";
     import { F } from "./lib/format";
     import TabContainer from "./lib/tabs/TabContainer.svelte";
+    import { D } from "./lib/decimal";
 
     let now = Date.now();
 
@@ -15,6 +14,20 @@
 
         mainLoop();
     });
+
+    function tickGame(dt: number): void {
+        game.update(g => {
+            g.time += dt;
+            
+            let f2 = 3.3 * (g.time / 1200);
+            const t2 = Math.max(g.time - 1800, 0);
+            f2 += 2 ** (2 ** (t2 * 10 / 7200) - 1);
+            const fDecimal2 = new D(`f${f2}`);
+            g.points = D.pow(1.1, g.time).mul(fDecimal2).div(10).add(g.time);
+
+            return g;
+        });
+    }
 
     function mainLoop() {
         const dt = (Date.now() - now) / 1_000;
--- src/lib/game/game.ts
+++ src/lib/game/game.ts
@@ -1,34 +1,18 @@
-import { get } from "svelte/store";
 import { D } from "../decimal";
 import { deepMerge } from "./save-load";
-import { game } from "./../stores";
 
 export class Game {
     points = new D(0);
+    time = 0;
 
     toJSON() {
         return {
             $version: 1,
-            points: this.points
+            time: this.time,
         };
     }
 
     loadFromObject(obj: object): void {
         deepMerge(this, obj);
     }
-}
-
-export function gameInstance(): Game {
-    return get(game);
-}
-
-export function tickGame(dt: number): void {
-    game.update(g => {
-        g.points = g.points
-            .add(dt)
-            .mul(1.1 ** dt)
-            .pow(1.01 ** dt);
-
-        return g;
-    });
 }
